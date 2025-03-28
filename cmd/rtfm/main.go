5ada4447ba8c0b29d9877ed518650ec1be6c93ea
Greg Brandt <brandt.greg@gmail.com>
2025-03-28 14:27:16 -0700

    WIP

--- cmd/rtfm/main.go
+++ cmd/rtfm/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+    // "flag"
+    "log/slog"
+    "os"
+    "github.com/brandtg/rtfm/internal/core"
+)
+
+func index(args []string) {
+    // TODO parse args
+    core.Index()
+}
+
+func find(args []string) {
+    // TODO parse args
+    core.Find()
+}
 
 func main() {
-    fmt.Println("Hello, world!")
+    if len(os.Args) < 2 {
+        slog.Error("expected subcommand")
+        os.Exit(1)
+    }
+    switch os.Args[1] {
+    case "index":
+        index(os.Args[2:])
+    case "find":
+        find(os.Args[2:])
+    default:
+        slog.Error("unknown command")
+        os.Exit(1)
+    }
 }
---
+++ internal/core/index.go
@@ -0,0 +1,9 @@
+package core
+
+import (
+    "log/slog"
+)
+
+func Index() {
+    slog.Info("Indexing...")
+}
