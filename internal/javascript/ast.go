00c29151245699f8a1f6a0ceabc28954d2c5be6a
Greg Brandt <brandt.greg@gmail.com>
2025-04-04 00:04:07 -0700

    WIP

--- internal/javascript/ast.go
+++ internal/javascript/ast.go
@@ -2,6 +2,7 @@ package javascript
 
 import (
 	"encoding/json"
+	"fmt"
 	"log/slog"
 	"os/exec"
 )
@@ -16,10 +17,58 @@ func parseAST(
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println(string(output))
 	var result map[string]any
 	if err := json.Unmarshal(output, &result); err != nil {
 		return nil, err
 	}
+
+	/*
+
+➜  rtfm git:(brandtg/jsdoc-ast) ✗ cat ~/Desktop/test.js
+
+var myFunctionVariable = () => console.log("hello world");
+
+function myFunction() {
+    console.log("Hello world");
+}
+
+	.program.body[] appears to be the list of AST nodes
+
+➜  rtfm git:(brandtg/jsdoc-ast) ✗ jq '.program.body[].type' tmp/ast.json
+"VariableDeclaration"
+"VariableDeclaration"
+"VariableDeclaration"
+"VariableDeclaration"
+"ExpressionStatement"
+
+➜  rtfm git:(brandtg/jsdoc-ast) ✗ jq '.program.body[].kind' tmp/ast.json
+"const"
+"const"
+"const"
+"const"
+null
+
+Some of this is comming from that wrapper script
+
+	const parser = require("@babel/parser");
+	const fs = require("fs");
+	const code = fs.readFileSync(process.argv[1], "utf8");
+	const ast = parser.parse(code, {
+		sourceType: "unambiguous",
+		plugins: ["jsx", "typescript", "classProperties", "decorators-legacy"],
+	});
+	console.log(JSON.stringify(ast, null, 2));
+
+	*/
+
+	// value, ok := result["keyName"].(string)
+	// if !ok {
+	//     // Handle the case where the value is not a string
+	//     return fmt.Errorf("keyName is not a string")
+	// }
+	// fmt.Println("Value:", value)
+
 	return &result, nil
 }
 
@@ -34,7 +83,7 @@ func DemoASTParser(
 	if err != nil {
 		return err
 	}
-	slog.Info("Parsed AST", "ast", ast)
+	slog.Debug("Parsed AST", "ast", ast)
 	// jsCode, err := os.ReadFile(path)
 	// if err != nil {
 	// 	return err
